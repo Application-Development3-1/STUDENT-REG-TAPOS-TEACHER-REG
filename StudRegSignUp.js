a29a24eab7df18540a91af3da5cb41e7492f2d04
kylatohahaha <146633619+kylatohahaha@users.noreply.github.com>
2023-10-30 01:55:43 +0800

    Add files via upload

---
+++ StudRegSignUp.js
@@ -0,0 +1,21 @@
+function checkPassword(){
+
+  let password = document.getElementById("password").value;
+
+  let reEnterPassword = document.getElementById("re-enterStudentPassword").value;
+
+  console.log(password, reEnterPassword);
+  let message = document.getElementById("message");
+
+  if(password.length != 0){
+    if(password==reEnterPassword){
+      message.textContent = "Password Match!";
+      message.style.backgroundColor = "#3ae374";
+    }
+    else{
+      message.textContent = "Password don't match!";
+      message.style.backgroundColor = "#ff4d4d";
+    }
+  }
+
+}
